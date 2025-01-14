bc8f684a6e2e177a121d9e03aa23b4fccb3c7f7b
azizaweb <azizaweb@mail.ru>
2025-01-14 22:23:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// let user = prompt( 'сколько тебе лет?')
+// if(user == 19){ alert('вы правы')
+// }else{
+//     alert('не спорь')
+// }
+// let user = prompt( 'сколько тебе лет?');
+//  if(user < 19){ 
+//     alert('это слишком мало');
+//  }else if ( user > 19){
+//     alert('это многовато');
+//  }else{
+//     alert('Верно');
+//  }
+
+
+
+
+// alert(true || false )
+// alert(false || true)
+// alert(true || false)
+// alert(false || false)
+
+// let hour = 12;
+// let isWEeekend = true;
+
+
+
+
+
+// let user = prompt('Как тебя зовут?');
+// let bukva = prompt('введите букву ')
+
+
+let user = prompt('Как тебя зовут?');
+let bukva = prompt('Введите букву');
+
+if (user.includes(bukva)) {
+    alert('В вашем имени присутствует буква "' + bukva + '"');
+} else {
+    alert('В вашем имени не присутствует буква "' + bukva + '"');
+}
+
